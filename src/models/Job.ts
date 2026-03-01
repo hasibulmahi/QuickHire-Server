0af1ac05f2b0ab4477d54c8b299659096d008175
Hasibul Mahi <hasibul15-14184@diu.edu.bd>
2026-03-01 14:32:39 +0600

    feat: add job models

---
+++ src/models/Job.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IJob extends Document {
+  _id: mongoose.Types.ObjectId;
+  title: string;
+  company: string;
+  location: string;
+  category: string;
+  description: string;
+  jobType: string;
+  tags: string[];
+  logo: string;
+  createdAt: Date;
+}
+
+const JobSchema = new Schema<IJob>(
+  {
+    title: { type: String, required: true, trim: true },
+    company: { type: String, required: true, trim: true },
+    location: { type: String, required: true, trim: true },
+    category: { type: String, required: true, trim: true, default: "General" },
+    description: { type: String, default: "" },
+    jobType: { type: String, required: true, trim: true, default: "Full Time" },
+    tags: { type: [String], default: [] },
+    logo: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
+
+// Use "createdAt" from timestamps
+JobSchema.set("toJSON", {
+  virtuals: false,
+  versionKey: false,
+  transform(_doc, ret) {
+    ret.id = ret._id.toString();
+    ret.companyName = ret.company;
+    ret.created_at = ret.createdAt;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.company;
+    delete ret.createdAt;
+    delete ret.updatedAt;
+  },
+});
+
+export const Job = mongoose.model<IJob>("Job", JobSchema);
